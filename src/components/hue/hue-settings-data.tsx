da950dde6695411e8213076a7d63ff7179696cb0
jazz <jazzbauer77@gmail.com>
2026-03-14 20:54:34 +0100

    a

---
+++ src/components/hue/hue-settings-data.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import HueScenes from "@/components/hue/hue-scenes";
+
+const HueSettingsData = () => {
+    return (
+        <div className="p-4 h-[400px]">
+            <HueScenes />
+        </div>
+    );
+};
+
+export default HueSettingsData;
