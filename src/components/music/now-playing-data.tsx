012aa6d9fe51eecb3b555f1a8d57cde79e796cbf
jazz <jazzbauer77@gmail.com>
2026-04-29 18:01:57 +0200

    Fix?

--- src/components/music/now-playing-data.tsx
+++ src/components/music/now-playing-data.tsx
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react";
 import NowPlaying from "@/components/music/now-playing";
 import { MonitoringData } from "@/types/monitor";
 import WebSocketData from "@/components/data/web-socket-data";
 
-const NowPlayingData = () => <WebSocketData<MonitoringData> Component={NowPlaying} />;
+const NowPlayingData = () => (
+    <WebSocketData<MonitoringData>
+        Component={({ socket }) => {
+            const [data, setData] = useState<MonitoringData | null>(null);
+
+            useEffect(() => {
+                if (socket.socket) {
+                    socket.socket.onmessage = (event) => {
+                        setData(JSON.parse(event.data));
+                    };
+                }
+            }, [socket.socket]);
+
+            return data ? <NowPlaying data={data} /> : <div>Loading...</div>;
+        }}
+    />
+);
 export default NowPlayingData;
